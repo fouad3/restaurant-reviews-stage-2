e27dfde5dc53418e664c5b2cfc521b25d56a7834
fouad3 <eng.fouad.ashraf@gmail.com>
2018-08-10 04:26:02 +0200

    refactor: refactor some code in main.js and main.bundle.js change the restaurant name header from h1 to h2.

--- app/js/main.js
+++ app/js/main.js
@@ -169,7 +169,7 @@ let createRestaurantHTML = (restaurant) => {
   image.setAttribute('alt',`${restaurant.name} Restaurant`);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
